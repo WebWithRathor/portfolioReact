23cef1063b56b20045359eb18527f7858d3da99f
WebWithRathor <devrajrathor143@gmail.com>
2024-07-15 22:37:07 +0530

    .

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='flex items-center justify-center bg-black'>
+      <h1 className='text-lg font-semibold text-white'>My Portfolio</h1>
+    </div>
   )
 }
 
