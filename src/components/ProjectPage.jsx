48f833bfb76ed3213a57a6b3a0b2dcc8879ae106
WebWithRathor <devrajrathor143@gmail.com>
2024-07-16 21:18:56 +0530

    project page

---
+++ src/components/ProjectPage.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+
+const ProjectPage = () => {
+  return (
+    <div className="projectpage px-20 pl-32 shrink-0 flex h-full">
+    <h1 className='text-9xl font-[Melodrama] font-medium pt-8 mr-20 text-[#6968B5]'>Projects</h1>
+    <div className="items flex">
+        <div className="project relative z-0 after:absolute after:h-full after:w-full after:bg-[#6968B5] after:bottom-0 after:z-[-1] after:scale-y-0 hover:after:scale-y-100 after:origin-bottom after:transition-transform before:duration-200 hover:text-white transition-all cursor-pointer h-full w-[9vw] flex flex-col items-center justify-center border-x border-black/[.2] font-semibold">
+            <h1 className='mt-20'>TAILWIND</h1>
+            <h1>CSS</h1>
+        </div>
+        <div className="project relative z-0 before:absolute before:h-1/2 before:w-full before:bg-[#6968B5] before:top-0 before:z-[-1] after:absolute after:h-1/2 after:w-full after:bg-[#6968B5] after:bottom-0 after:z-[-1] before:scale-y-0 after:scale-y-0 hover:after:scale-y-100 hover:before:scale-y-100 before:origin-top after:origin-bottom after:transition-transform before:transition-transform after:duration-200 before:duration-200 hover:text-white transition-all cursor-pointer h-full w-[9vw] flex flex-col items-center justify-center border-r border-black/[.2] font-semibold">
+            <h1 className='mt-20'>TAILWIND</h1>
+            <h1>CSS</h1>
+        </div>
+        <div className="project relative z-0 before:absolute before:h-1/3 before:w-full before:bg-[#6968B5] before:top-0 before:z-[-1] after:absolute after:h-2/3 after:w-full after:bg-[#6968B5] after:bottom-0 after:z-[-1] before:scale-y-0 after:scale-y-0 hover:after:scale-y-100 hover:before:scale-y-100 before:origin-top after:origin-bottom after:transition-transform before:transition-transform after:duration-200 before:duration-200 hover:text-white transition-all cursor-pointer h-full w-[9vw] flex flex-col items-center justify-center border-r border-black/[.2] font-semibold">
+            <h1 className='mt-20'>TAILWIND</h1>
+            <h1>CSS</h1>
+        </div>
+        <div className="project relative z-0 before:absolute before:h-1/4 before:w-full before:bg-[#6968B5] before:top-0 before:z-[-1] after:absolute after:h-3/4 after:w-full after:bg-[#6968B5] after:bottom-0 after:z-[-1] before:scale-y-0 after:scale-y-0 hover:after:scale-y-100 hover:before:scale-y-100 before:origin-top after:origin-bottom after:transition-transform before:transition-transform after:duration-200 before:duration-200 hover:text-white transition-all cursor-pointer h-full w-[9vw] flex flex-col items-center justify-center border-r border-black/[.2] font-semibold">
+            <h1 className='mt-20'>TAILWIND</h1>
+            <h1>CSS</h1>
+        </div>
+        <div className="project relative z-0 before:absolute before:h-1/5 before:w-full before:bg-[#6968B5] before:top-0 before:z-[-1] after:absolute after:h-4/5 after:w-full after:bg-[#6968B5] after:bottom-0 after:z-[-1] before:scale-y-0 after:scale-y-0 hover:after:scale-y-100 hover:before:scale-y-100 before:origin-top after:origin-bottom after:transition-transform before:transition-transform after:duration-200 before:duration-200 hover:text-white transition-all cursor-pointer h-full w-[9vw] flex flex-col items-center justify-center border-r border-black/[.2] font-semibold">
+            <h1 className='mt-20'>TAILWIND</h1>
+            <h1>CSS</h1>
+        </div>
+        <div className="project relative z-0 before:absolute before:h-1/6 before:w-full before:bg-[#6968B5] before:top-0 before:z-[-1] after:absolute after:h-5/6 after:w-full after:bg-[#6968B5] after:bottom-0 after:z-[-1] before:scale-y-0 after:scale-y-0 hover:after:scale-y-100 hover:before:scale-y-100 before:origin-top after:origin-bottom after:transition-transform before:transition-transform after:duration-200 before:duration-200 hover:text-white transition-all cursor-pointer h-full w-[9vw] flex flex-col items-center justify-center border-r border-black/[.2] font-semibold">
+            <h1 className='mt-20'>TAILWIND</h1>
+            <h1>CSS</h1>
+        </div>
+       
+    </div>
+</div>
+  )
+}
+
+export default ProjectPage
