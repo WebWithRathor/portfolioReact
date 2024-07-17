c0c43387952e5bdc2ccab7e4794e746ea4fbc07a
WebWithRathor <devrajrathor143@gmail.com>
2024-07-17 23:30:16 +0530

    .

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,16 +1,49 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import Homepage from './Homepage'
 import Nav from './Nav'
 import SkillsPage from './SkillsPage'
 import ProjectPage from './ProjectPage'
+import AboutPage from './AboutPage'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
+import gsap from 'gsap'
+import { useGSAP } from '@gsap/react'
+import { motion, useScroll } from 'framer-motion'
+import ContactUs from './ContactUs'
 
 const Home = () => {
+
+  const main = useRef(null)
+  
+
+
+  gsap.registerPlugin(ScrollTrigger)
+
+  useGSAP(() => {
+    gsap.to('.page', {
+      translateX: "-400vw",
+      ease: 'linear',
+      duration: 30,
+      scrollTrigger:{
+        scroller:"body",
+        trigger: ".main",
+        start: "top 0%",
+        end: "top -200%",
+        pin:true,
+        scrub:2
+      }
+    })
+
+  }, [])
   return (
-    <div className='flex font-[ClashDisplay] h-screen overflow-x-auto overflow-hidden items-center text-zinc-700 bg-gradient-to-b from-[#EDE9C9] to-white'>
-      <Nav/>
-      <Homepage/>
-      <SkillsPage/>
-      <ProjectPage/>
+    <div ref={main} className="main scrollbar-none h-screen relative">
+      <div className='flex parent font-[ClashDisplay] h-screen  overflow-hidden items-center text-zinc-700 '>
+        <Nav />
+        <Homepage />
+        <AboutPage/>
+        <SkillsPage />
+        <ProjectPage />
+        <ContactUs/>
+      </div>
     </div>
   )
 }
--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 
 const Homepage = () => {
     return (
-        <div className="homepage shrink-0 p-10 z-0  relative flex h-full w-[95vw]">
-            <p className='ml-auto w-[45%] font-[ClashDisplay] tracking-wider'>As a second-year B.Tech student specializing in MERN stack and proficient in C language, I excel in both front-end and back-end development. Passionate about coding, I create dynamic, responsive web applications and thrive in collaborative projects, continuously learning to deliver innovative and impactful solutions.</p>
+        <div className="page shrink-0 p-10 z-0  relative h-full w-screen bg-gradient-to-b from-[#EDE9C9] to-white">
+            <p className='ml-auto w-1/2 font-[ClashDisplay] text-lg tracking-wider text-right'>As a second-year B.Tech student specializing in MERN stack and proficient in C language, I excel in both front-end and back-end development. Passionate about coding, I create dynamic, responsive web applications and thrive in collaborative projects, continuously learning to deliver innovative and impactful solutions.</p>
+            <div className="ml-auto w-max px-5 rounded-md text-[#fcfbec] font-medium text-xl mt-4 py-3 bg-[#6968B5]">Get Resume</div>
             <h1 className='absolute top-[96%]  -translate-y-full  text-[8vw] leading-none font-medium font-[ClashDisplay]'>DEVRAJ <span className='text-[#6968B5] font-[Melodrama] ml-3 font-semibold'>RATHOR</span></h1>
         </div>
     )
