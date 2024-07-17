4136811f57e366200f64ab0a65b735fa121ebe78
WebWithRathor <devrajrathor143@gmail.com>
2024-07-17 23:30:10 +0530

    .

---
+++ src/components/AboutPage.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import InfiniteBar from './InfiniteBar'
+import video from '../assets/Devraj rathor portfolio.png'
+import gsap from 'gsap'
+import { motion, useMotionValue, useSpring } from "framer-motion"
+
+const AboutPage = ({ X }) => {
+
+    const x = useMotionValue(0)
+    const spring = useSpring(x)
+    useSpring(x, { stiffness: 1000, damping: 10 })
+
+
+    return (
+        <>
+            <div className="px-32 page shrink-0 flex relative z-0 bg-gradient-to-b from-[#EDE9C9] to-white">
+                <InfiniteBar />
+                <h1 className='text-9xl ml-32 font-medium pt-72 leading-[7rem] w-max shrink-0'>Hi !
+                    <motion.div
+                        dragConstraints={{ left: 0, right: 0, top: 0, bottom: 0 }}
+                        drag dragTransition={{ bounceStiffness: 500, bounceDamping: 20 }}
+                        style={{ x }} className='text-4xl inline-block tracking-wide ml-3'> I'm  FullStack Devloper</motion.div>
+                </h1>
+                <div className="box flex items-center absolute w-80 right-14 top-48 justify-center">
+                    <img className='h-80 mix-blend-darken absolute shrink-0  animate-[spin_15s_linear_infinite]' src={video} alt="" />
+                    <i className="fa-solid fa-code text-4xl"></i>
+                </div>
+
+                <p className='pointer-events-none pt-[60vh] w-[50vw] shrink-0 -ml-80 text-xl  tracking-wider leading-8 ' >Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam, aliquid inventore! Cumque quos modi, unde non eius obcaecati possimus error doloremque tempore provident nisi, nostrum, eligendi dolore corrupti saepe commodi sint illum ullam consequatur ipsum ab culpa deserunt facere. Ea.</p>
+            </div>
+        </>
+    )
+}
+
+export default AboutPage
---
+++ src/components/ContactUs.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Heading from './partials/Heading'
+
+const ContactUs = () => {
+
+
+    return (
+        <div className='page h-full flex py-12 bg-gradient-to-b from-[#EDE9C9] to-white'>
+            <Heading head={'Contact Us'}  mt={'auto'}/>
+        </div>
+    )
+}
+
+export default ContactUs
