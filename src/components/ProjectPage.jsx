e4d82250508bf6929070294f9f68a4235408e59d
WebWithRathor <devrajrathor143@gmail.com>
2024-07-18 21:28:06 +0530

    .

--- src/components/ProjectPage.jsx
+++ src/components/ProjectPage.jsx
@@ -1,34 +1,84 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import Heading from './partials/Heading'
 
 const ProjectPage = () => {
+
+    const ref = useRef([])
+    const pushRef = (el) => ref.current.push(el);
+    useEffect(() => {
+        if(ref.current) console.log(ref.current)
+      }, [ref])
+      
+
   return (
     <div className="page px-20 bg-gradient-to-b from-[#EDE9C9] to-white pl-32 shrink-0 flex h-full">
         <Heading head={'Projects'} mt={12}/>
     <div className="items flex">
-        <div className="project relative z-0 after:absolute after:h-full after:w-full after:bg-[#6968B5] after:bottom-0 after:z-[-1] after:scale-y-0 hover:after:scale-y-100 after:origin-bottom after:transition-transform before:duration-200 hover:text-white transition-all cursor-pointer h-full w-[9vw] flex flex-col items-center justify-center border-x border-black/[.2] font-semibold">
-            <h1 className='mt-20'>TAILWIND</h1>
-            <h1>CSS</h1>
+        <div ref={pushRef} className="project relative z-0 after:absolute after:h-full after:w-full after:bg-[#6968B5] after:bottom-0 after:z-[-1] after:scale-y-0 hover:after:scale-y-100 after:origin-top after:transition-transform hover:text-white transition-all cursor-pointer h-full w-[10vw] flex flex-col items-center justify-center border-x border-black/[.2] font-medium">
+            <div className=" text-center -rotate-90">
+            <h1 className='text-xl mb-1 tracking-wide'>Internshala</h1>
+            <h1 className='text-lg tracking-wide'>Clone(FullStack)</h1>
+            </div>
         </div>
-        <div className="project relative z-0 before:absolute before:h-1/2 before:w-full before:bg-[#6968B5] before:top-0 before:z-[-1] after:absolute after:h-1/2 after:w-full after:bg-[#6968B5] after:bottom-0 after:z-[-1] before:scale-y-0 after:scale-y-0 hover:after:scale-y-100 hover:before:scale-y-100 before:origin-top after:origin-bottom after:transition-transform before:transition-transform after:duration-200 before:duration-200 hover:text-white transition-all cursor-pointer h-full w-[9vw] flex flex-col items-center justify-center border-r border-black/[.2] font-semibold">
-            <h1 className='mt-20'>TAILWIND</h1>
-            <h1>CSS</h1>
+        <div ref={pushRef} className="project relative z-0 after:absolute after:h-1/5 before:h-4/5 after:w-full after:bg-[#6968B5] after:bottom-0 after:z-[-1] after:scale-y-0 hover:after:scale-y-100 after:origin-bottom after:transition-transform before:absolute  before:w-full before:bg-[#6968B5] before:top-0 before:z-[-1] before:scale-y-0 hover:before:scale-y-100 before:origin-top  before:transition-transform hover:text-white transition-all cursor-pointer h-full w-[10vw] flex flex-col items-center justify-center border-r border-black/[.2] font-medium">
+            <div className=" text-center -rotate-90">
+            <h1 className='text-xl mb-1 tracking-wide'>Movie-app</h1>
+            <h1 className='text-lg tracking-wide w-max'>(React-api-integration)</h1>
+            </div>
         </div>
-        <div className="project relative z-0 before:absolute before:h-1/3 before:w-full before:bg-[#6968B5] before:top-0 before:z-[-1] after:absolute after:h-2/3 after:w-full after:bg-[#6968B5] after:bottom-0 after:z-[-1] before:scale-y-0 after:scale-y-0 hover:after:scale-y-100 hover:before:scale-y-100 before:origin-top after:origin-bottom after:transition-transform before:transition-transform after:duration-200 before:duration-200 hover:text-white transition-all cursor-pointer h-full w-[9vw] flex flex-col items-center justify-center border-r border-black/[.2] font-semibold">
-            <h1 className='mt-20'>TAILWIND</h1>
-            <h1>CSS</h1>
+        <div ref={pushRef} className="project relative z-0 after:absolute after:h-2/5 before:h-3/5 after:w-full after:bg-[#6968B5] after:bottom-0 after:z-[-1] after:scale-y-0 hover:after:scale-y-100 after:origin-bottom after:transition-transform before:absolute  before:w-full before:bg-[#6968B5] before:top-0 before:z-[-1] before:scale-y-0 hover:before:scale-y-100 before:origin-top  before:transition-transform hover:text-white transition-all cursor-pointer h-full w-[10vw] flex flex-col items-center justify-center border-r border-black/[.2] font-medium">
+            <div className=" text-center -rotate-90">
+            <h1 className='text-xl mb-1 tracking-wide'>Intstagram</h1>
+            <h1 className='text-lg tracking-wide'>Clone(FullStack)</h1>
+            </div>
         </div>
-        <div className="project relative z-0 before:absolute before:h-1/4 before:w-full before:bg-[#6968B5] before:top-0 before:z-[-1] after:absolute after:h-3/4 after:w-full after:bg-[#6968B5] after:bottom-0 after:z-[-1] before:scale-y-0 after:scale-y-0 hover:after:scale-y-100 hover:before:scale-y-100 before:origin-top after:origin-bottom after:transition-transform before:transition-transform after:duration-200 before:duration-200 hover:text-white transition-all cursor-pointer h-full w-[9vw] flex flex-col items-center justify-center border-r border-black/[.2] font-semibold">
-            <h1 className='mt-20'>TAILWIND</h1>
-            <h1>CSS</h1>
+        <div ref={pushRef} className="project relative z-0 after:absolute after:h-3/5 before:h-2/5 after:w-full after:bg-[#6968B5] after:bottom-0 after:z-[-1] after:scale-y-0 hover:after:scale-y-100 after:origin-bottom after:transition-transform before:absolute  before:w-full before:bg-[#6968B5] before:top-0 before:z-[-1] before:scale-y-0 hover:before:scale-y-100 before:origin-top  before:transition-transform hover:text-white transition-all cursor-pointer h-full w-[10vw] flex flex-col items-center justify-center border-r border-black/[.2] font-medium">
+            <div className=" text-center -rotate-90">
+            <h1 className='text-xl mb-1 tracking-wide'>Gallery-app</h1>
+            <h1 className='text-lg tracking-wide w-max'>(React-api-integration)</h1>
+            </div>
         </div>
-        <div className="project relative z-0 before:absolute before:h-1/5 before:w-full before:bg-[#6968B5] before:top-0 before:z-[-1] after:absolute after:h-4/5 after:w-full after:bg-[#6968B5] after:bottom-0 after:z-[-1] before:scale-y-0 after:scale-y-0 hover:after:scale-y-100 hover:before:scale-y-100 before:origin-top after:origin-bottom after:transition-transform before:transition-transform after:duration-200 before:duration-200 hover:text-white transition-all cursor-pointer h-full w-[9vw] flex flex-col items-center justify-center border-r border-black/[.2] font-semibold">
-            <h1 className='mt-20'>TAILWIND</h1>
-            <h1>CSS</h1>
+        <div ref={pushRef} className="project relative z-0 after:absolute after:h-4/5 before:h-1/5 after:w-full after:bg-[#6968B5] after:bottom-0 after:z-[-1] after:scale-y-0 hover:after:scale-y-100 after:origin-bottom after:transition-transform before:absolute  before:w-full before:bg-[#6968B5] before:top-0 before:z-[-1] before:scale-y-0 hover:before:scale-y-100 before:origin-top  before:transition-transform hover:text-white transition-all cursor-pointer h-full w-[10vw] flex flex-col items-center justify-center border-r border-black/[.2] font-medium">
+            <div className=" text-center -rotate-90">
+            <h1 className='text-xl mb-1 tracking-wide w-max'>Mini Whatsapp(chatiing)</h1>
+            <h1 className='text-lg tracking-wide'>Clone(FullStack)</h1>
+            </div>
         </div>
-        <div className="project relative z-0 before:absolute before:h-1/6 before:w-full before:bg-[#6968B5] before:top-0 before:z-[-1] after:absolute after:h-5/6 after:w-full after:bg-[#6968B5] after:bottom-0 after:z-[-1] before:scale-y-0 after:scale-y-0 hover:after:scale-y-100 hover:before:scale-y-100 before:origin-top after:origin-bottom after:transition-transform before:transition-transform after:duration-200 before:duration-200 hover:text-white transition-all cursor-pointer h-full w-[9vw] flex flex-col items-center justify-center border-r border-black/[.2] font-semibold">
-            <h1 className='mt-20'>TAILWIND</h1>
-            <h1>CSS</h1>
+        <div ref={pushRef} className="project relative z-0 after:absolute after:h-full after:w-full after:bg-[#6968B5] after:bottom-0 after:z-[-1] after:scale-y-0 hover:after:scale-y-100 after:origin-bottom after:transition-transform before:duration-200 hover:text-white transition-all cursor-pointer h-full w-[10vw] flex flex-col items-center justify-center border-r border-black/[.2] font-medium">
+            <div className=" text-center -rotate-90">
+            <h1 className='text-xl mb-1 tracking-wide'>Mini Youtube</h1>
+            <h1 className='text-lg tracking-wide'>Clone(FullStack)</h1>
+            </div>
+        </div>
+        <div ref={pushRef} className="project relative z-0 after:absolute after:h-4/5 before:h-1/5 after:w-full after:bg-[#6968B5] after:bottom-0 after:z-[-1] after:scale-y-0 hover:after:scale-y-100 after:origin-bottom after:transition-transform before:absolute  before:w-full before:bg-[#6968B5] before:top-0 before:z-[-1] before:scale-y-0 hover:before:scale-y-100 before:origin-top  before:transition-transform hover:text-white transition-all cursor-pointer h-full w-[10vw] flex flex-col items-center justify-center border-r border-black/[.2] font-medium">
+            <div className=" text-center -rotate-90">
+            <h1 className='text-xl mb-1 tracking-wide'>FakeStore-app</h1>
+            <h1 className='text-lg tracking-wide w-max'>(React-api-integration)</h1>
+            </div>
+        </div>
+        <div ref={pushRef} className="project relative z-0 after:absolute after:h-3/5 before:h-2/5 after:w-full after:bg-[#6968B5] after:bottom-0 after:z-[-1] after:scale-y-0 hover:after:scale-y-100 after:origin-bottom after:transition-transform before:absolute  before:w-full before:bg-[#6968B5] before:top-0 before:z-[-1] before:scale-y-0 hover:before:scale-y-100 before:origin-top  before:transition-transform hover:text-white transition-all cursor-pointer h-full w-[10vw] flex flex-col items-center justify-center border-r border-black/[.2] font-medium">
+            <div className=" text-center -rotate-90">
+            <h1 className='text-xl mb-1 tracking-wide'>Quiz-app</h1>
+            <h1 className='text-lg tracking-wide w-max'>(React-api-integration)</h1>
+            </div>
+        </div>
+        <div ref={pushRef} className="project relative z-0 after:absolute after:h-2/5 before:h-3/5 after:w-full after:bg-[#6968B5] after:bottom-0 after:z-[-1] after:scale-y-0 hover:after:scale-y-100 after:origin-bottom after:transition-transform before:absolute  before:w-full before:bg-[#6968B5] before:top-0 before:z-[-1] before:scale-y-0 hover:before:scale-y-100 before:origin-top  before:transition-transform hover:text-white transition-all cursor-pointer h-full w-[10vw] flex flex-col items-center justify-center border-r border-black/[.2] font-medium">
+            <div className=" text-center -rotate-90">
+            <h1 className='text-xl mb-1 tracking-wide'>Silena</h1>
+            <h1 className='text-lg tracking-wide'>(FrontEnd)</h1>
+            </div>
+        </div>
+        <div ref={pushRef} className="project relative z-0 after:absolute after:h-1/5 before:h-4/5 after:w-full after:bg-[#6968B5] after:bottom-0 after:z-[-1] after:scale-y-0 hover:after:scale-y-100 after:origin-bottom after:transition-transform before:absolute  before:w-full before:bg-[#6968B5] before:top-0 before:z-[-1] before:scale-y-0 hover:before:scale-y-100 before:origin-top  before:transition-transform hover:text-white transition-all cursor-pointer h-full w-[10vw] flex flex-col items-center justify-center border-r border-black/[.2] font-medium">
+            <div className=" text-center -rotate-90">
+            <h1 className='text-xl mb-1 tracking-wide'>Ecommerce</h1>
+            <h1 className='text-lg tracking-wide'>(FullStack)</h1>
+            </div>
+        </div>
+        <div ref={pushRef} className="project relative z-0 after:absolute after:h-full after:w-full after:bg-[#6968B5] after:bottom-0 after:z-[-1] after:scale-y-0 hover:after:scale-y-100 after:origin-top after:transition-transform hover:text-white transition-all cursor-pointer h-full w-[10vw] flex flex-col items-center justify-center border-r border-black/[.2] font-medium">
+            <div className=" text-center -rotate-90">
+            <h1 className='text-xl mb-1 tracking-wide'>Magma</h1>
+            <h1 className='text-lg tracking-wide'>(FrontEnd)</h1>
+            </div>
         </div>
        
     </div>
