f8eba43caf127431e3d814278adacd11de08a4c9
WebWithRathor <devrajrathor143@gmail.com>
2024-07-19 22:36:54 +0530

    .

--- src/components/ProjectPage.jsx
+++ src/components/ProjectPage.jsx
@@ -11,9 +11,9 @@ const ProjectPage = () => {
       
 
   return (
-    <div className="page px-20 bg-gradient-to-b from-[#EDE9C9] to-white pl-32 shrink-0 flex h-full">
+    <div className="page px-20 bg-gradient-to-b from-[#EDE9C9] to-white shrink-0 flex h-full">
         <Heading head={'Projects'} mt={12}/>
-    <div className="items flex">
+    <div className="items flex ml-14">
         <div ref={pushRef} className="project relative z-0 after:absolute after:h-full after:w-full after:bg-[#6968B5] after:bottom-0 after:z-[-1] after:scale-y-0 hover:after:scale-y-100 after:origin-top after:transition-transform hover:text-white transition-all cursor-pointer h-full w-[10vw] flex flex-col items-center justify-center border-x border-black/[.2] font-medium">
             <div className=" text-center -rotate-90">
             <h1 className='text-xl mb-1 tracking-wide'>Internshala</h1>
--- src/components/SkillsPage.jsx
+++ src/components/SkillsPage.jsx
@@ -1,47 +1,52 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import Heading from './partials/Heading'
+import Cube from './3dShapes/Cube'
+import { useGSAP } from '@gsap/react';
+import gsap from 'gsap';
 
 const SkillsPage = () => {
+    const page = useRef(null);
+    const [rotateX, setRotateX] = useState(-10)
+    const [rotateY, setRotateY] = useState(-10)
+    const threeRotate = function (e, elem) {
+        setRotateY(Math.floor(((page.current.clientWidth / 2 - e.clientX) / 30)))
+        setRotateX(Math.floor((-(page.current.clientHeight / 2 - e.clientY) / 30)))
+    }
+    useGSAP(() => {
+            gsap.to('.cube-container',{
+                top:'100%',
+                duration:20,
+                delay:5
+            })
+    })
+
     return (
-        <div className="page pl-10 py-12 px-20 shrink-0 flex h-full bg-gradient-to-b from-[#EDE9C9] to-white">
-            <Heading head={'Skills'} mt={'auto'}/>
-            <div className="skills flex w-[65vw] gap-5 flex-wrap">
-                <div className="skill flex-1  rounded-3xl p-10  shrink-0  bg-gradient-to-t from-[#CDC7E5] via-transparent to-transparent">
-                    <h1 className='text-5xl'>Backend Technologies</h1>
-                    <div className="flex gap-5 mt-5">
-                        <img src="https://skillicons.dev/icons?i=nodejs" className='h-16' alt="nodejs logo" />
-                        <img src="https://skillicons.dev/icons?i=express" className='h-16' alt="express logo" />
-                        <img src="https://cdn.simpleicons.org/postman/FF6C37" className='h-16' alt="postman logo" />
-                        <img src="https://skillicons.dev/icons?i=mongodb" className='h-16' alt="mongodb logo" />
-                    </div>
+        <div ref={page} onMouseMove={e => threeRotate(e)} className="page pl-10 relative py-12 px-20 shrink-0 flex flex-col w-screen items-center justify-center h-full bg-gradient-to-b from-[#EDE9C9] to-white">
+            <Heading head={'Skills'} mt={'auto'} />
+            <p className='w-1/2 text-center'>Infinite Scroll | nodemailer | Razorpay</p>
+            <div className="w-full cube-container h-screen top-full absolute">
+                <Cube top={'12%'} rotateX={rotateX} alt={'nodejs logo'} img={'https://skillicons.dev/icons?i=nodejs'} rotateY={rotateY} left={"10%"} />
+                <Cube top={'43%'} rotateX={rotateX} alt={'express logo'} img={'https://skillicons.dev/icons?i=express'} rotateY={rotateY} left={"25%"} />
+                <Cube top={'75%'} rotateX={rotateX} alt={'postman logo'} img={'https://cdn.simpleicons.org/postman/FF6C37'} rotateY={rotateY} left={"10%"} />
+                <Cube top={'13%'} rotateX={rotateX} alt={'mongodb logo'} img={'https://skillicons.dev/icons?i=mongodb'} rotateY={rotateY} left={"45%"} />
+                <Cube top={'30%'} rotateX={rotateX} alt={'html5 logo'} img={'https://skillicons.dev/icons?i=html'} rotateY={rotateY} left={"82%"} />
+                <Cube top={'70%'} rotateX={rotateX} alt={'bootstrap logo'} img={'https://cdn.simpleicons.org/bootstrap/7952B3'} rotateY={rotateY} left={"62%"} />
+                <Cube top={'90%'} rotateX={rotateX} alt={'css3 logo'} img={'https://skillicons.dev/icons?i=css'} rotateY={rotateY} left={"85%"} />
+                <Cube top={'135%'} rotateX={rotateX} alt={'tailwind logo'} img={'https://skillicons.dev/icons?i=tailwind'} rotateY={rotateY} left={"10%"} />
+                <Cube top={'103%'} rotateX={rotateX} alt={'react logo'} img={'https://skillicons.dev/icons?i=react'} rotateY={rotateY} left={"35%"} />
+                <Cube top={'130%'} rotateX={rotateX} alt={'Next.js logo'} img={'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nextjs/nextjs-original.svg'} rotateY={rotateY} left={"72%"} />
+                <Cube top={'163%'} rotateX={rotateX} alt={'redux logo'} img={'https://skillicons.dev/icons?i=redux'} rotateY={rotateY} left={"28%"} />
+                <Cube top={'175%'} rotateX={rotateX} alt={'JS logo'} img={'https://skillicons.dev/icons?i=js'} rotateY={rotateY} left={"80%"} />
+                <Cube top={'195%'} rotateX={rotateX} alt={'C logo'} img={'https://skillicons.dev/icons?i=c'} rotateY={rotateY} left={"10%"} />
+                <Cube top={'195%'} rotateX={rotateX} alt={'Arduino logo'} img={'https://skillicons.dev/icons?i=arduino'} rotateY={rotateY} left={"55%"} />
 
-                </div>
-                <div className="skill flex-1 rounded-3xl h-max p-10 shrink-0  bg-gradient-to-t from-[#CDC7E5] via-transparent to-transparent">
-                    <h1 className='text-5xl'>FrontEnd Technologies </h1>
-                    <div className="flex gap-5 flex-wrap mt-5">
-                        <img src="https://skillicons.dev/icons?i=html" className='h-16' alt="html5 logo" />
-                        <img src="https://cdn.simpleicons.org/bootstrap/7952B3" className='h-16' alt="bootstrap logo" />
-                        <img src="https://skillicons.dev/icons?i=css" className='h-16' alt="css3 logo" />
-                        <img src="https://skillicons.dev/icons?i=tailwind" className='h-16' alt="tailwindcss logo" />
-                        <img src="https://skillicons.dev/icons?i=react" className='h-16' alt="react logo" />
-                        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nextjs/nextjs-original.svg" className='h-16' alt="nextjs logo" />
-                        <img src="https://skillicons.dev/icons?i=redux" className='h-16' alt="redux logo" />
-                    </div>
+                <Cube top={'242%'} rotateX={rotateX} alt={'Git logo'} img={'https://skillicons.dev/icons?i=git'} rotateY={rotateY} left={"60%"} />
+                <Cube top={'243%'} rotateX={rotateX} alt={'googleCloud logo'} img={'https://skillicons.dev/icons?i=gcp'} rotateY={rotateY} left={"25%"} />
+                <Cube top={'275%'} rotateX={rotateX} alt={'VsCode logo'} img={'https://skillicons.dev/icons?i=vscode'} rotateY={rotateY} left={"10%"} />
+                <Cube top={'280%'} rotateX={rotateX} alt={'Github logo'} img={'https://skillicons.dev/icons?i=github'} rotateY={rotateY} left={"75%"} />
+                <Cube top={'230%'} rotateX={rotateX} alt={'Figma logo'} img={'https://skillicons.dev/icons?i=figma'} rotateY={rotateY} left={"82%"} />
+                <Cube top={'230%'} rotateX={rotateX} alt={'Figma logo'} img={'https://skillicons.dev/icons?i=figma'} rotateY={rotateY} left={"82%"} />
 
-                </div>
-                <div className="skill w-full rounded-3xl pt-10 px-10 h-max py-8 shrink-0  bg-gradient-to-t from-[#CDC7E5] via-transparent to-transparent">
-                    <h1 className='text-6xl'>Other's</h1>
-                    <div className='flex flex-wrap gap-5 mt-12'>
-                        <img src="https://skillicons.dev/icons?i=js" className='h-16' alt="javascript logo" />
-                        <img src="https://skillicons.dev/icons?i=c" className='h-16' alt="c logo" />
-                        <img src="https://skillicons.dev/icons?i=arduino" className='h-16' alt="arduino logo" />
-                        <img src="https://skillicons.dev/icons?i=git" className='h-16' alt="git logo" />
-                        <img src="https://skillicons.dev/icons?i=gcp" className='h-16' alt="googlecloud logo" />
-                        <img src="https://skillicons.dev/icons?i=vscode" className='h-16' alt="vscode logo" />
-                        <img src="https://skillicons.dev/icons?i=github" className='h-16' alt="github logo" />
-                        <img src="https://skillicons.dev/icons?i=figma" className='h-16' alt="figma logo" />
-                    </div>
-                </div>
             </div>
         </div>
     )
