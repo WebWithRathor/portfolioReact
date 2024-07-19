952d6d45a3c994b1d19786c4cbe896cb181a081a
WebWithRathor <devrajrathor143@gmail.com>
2024-07-19 22:37:08 +0530

    3d Sapes added

---
+++ src/components/3dShapes/Cube.jsx
@@ -0,0 +1,27 @@
+import { useGSAP } from '@gsap/react'
+import gsap from 'gsap'
+import React from 'react'
+
+const Cube = ({ top, left ,rotateX,rotateY,img,alt }) => {
+
+    useGSAP(() => {
+        gsap.to('.cube', {rotate:gsap.utils.random(-5,5,1),rotateX,rotateY})
+    },[rotateX,rotateY])
+
+    return (
+        <>
+            <div style={{ top, left }} className={`cube absolute  origin-center flex items-center  cursor-pointer justify-center`}>
+                <img src={img} className='h-20 transition-all duration-300' alt={alt} />
+                <div className="bottom absolute h-40 aspect-square  "></div>
+                <div className="top absolute h-40 aspect-square "></div>
+                <div className="right absolute h-40 aspect-square "></div>
+                <div className="back absolute h-40 aspect-square "></div>
+                <div className="bottom absolute h-40 aspect-square "></div>
+            <div className="h-40 w-40 absolute shadow-[0px_0px_50px_10px_#6866B5] shadowcube"></div>
+                <div className="left absolute h-40 aspect-square "></div>
+            </div>
+        </>
+    )
+}
+
+export default Cube
--- src/components/partials/Heading.jsx
+++ src/components/partials/Heading.jsx
@@ -2,13 +2,12 @@ import { motion, useMotionValue, useSpring } from 'framer-motion'
 import React from 'react'
 
 const Heading = ({ head,mt,mb ,mr }) => {
-    console.log(mb,mt);
     const x = useMotionValue(0)
     useSpring(x, { stiffness: 1000, damping: 10 })
     return (
             <motion.h1 dragConstraints={{ left: 0, right: 0, top: 0, bottom: 0 }}
                 drag dragTransition={{ bounceStiffness: 500, bounceDamping: 20 }}
-                style={{ x }} className={`text-9xl w-max font-[Melodrama] font-medium mb-${mb} mr-${mr ? '0' : '24'} text-[#6968B5] mt-${mt}`}>{head}</motion.h1>
+                style={{ x }} className={`text-9xl w-max font-[Melodrama] font-medium mb-${mb} mr-${mr ? '' : '24'} text-[#6968B5] mt-${mt}`}>{head}</motion.h1>
 
     )
 }
