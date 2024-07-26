c35a788a7a2c9bcd9c699904bfce1b20ff55c06d
WebWithRathor <devrajrathor143@gmail.com>
2024-07-26 22:54:06 +0530

    animation

--- src/components/AboutPage.jsx
+++ src/components/AboutPage.jsx
@@ -13,7 +13,7 @@ const AboutPage = ({ X }) => {
 
     return (
         <>
-            <div className="px-32 page shrink-0 flex relative z-0 bg-gradient-to-b from-[#EDE9C9] to-white">
+            <div className="px-32 page aboutpage shrink-0 flex relative z-0 bg-gradient-to-b from-[#EDE9C9] to-white">
                 <InfiniteBar text={'About Me'} height={'60vh'} rotate={'6deg'} />
                 <h1 className='text-9xl ml-32 font-medium pt-72 leading-[7rem] w-max shrink-0'>Hi !
                     <motion.div
@@ -27,11 +27,11 @@ const AboutPage = ({ X }) => {
                 </div>
 
                 <p className='pointer-events-none pt-[60vh] w-[50vw] shrink-0 -ml-80 text-xl  tracking-wider leading-8 ' > A passionate Bachelor of Technology (BTech) student in Computer
- Science and Engineering (CSE) and full-stack MERN web developer,
- proficient in Next.js and Figma. Skilled in creating dynamic and
- responsive applications using modern web technologies. Continuously
- improving skills in Figma for enhanced design capabilities. Eager to
- innovate and excel in the tech industry.</p>
+                    Science and Engineering (CSE) and full-stack MERN web developer,
+                    proficient in Next.js and Figma. Skilled in creating dynamic and
+                    responsive applications using modern web technologies. Continuously
+                    improving skills in Figma for enhanced design capabilities. Eager to
+                    innovate and excel in the tech industry.</p>
             </div>
         </>
     )
