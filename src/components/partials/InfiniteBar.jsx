d3f20894d24a58bbfccd44e60b9d6f1fd760f306
WebWithRathor <devrajrathor143@gmail.com>
2024-07-27 20:48:49 +0530

    .

--- src/components/partials/InfiniteBar.jsx
+++ src/components/partials/InfiniteBar.jsx
@@ -24,7 +24,7 @@ const InfiniteBar = ({ text, height, dir, rotate }) => {
 
 
     return (
-        <div style={{ rotate: `${rotate}` }} className={`infinite scrollbar-none shrink-0 flex flex-col w-[10vw] mx-10   bg-[#7776bc] text-[#fffbdb] overflow-hidden justify-center items-center h-[110vh] `}>
+        <div style={{ rotate: `${rotate}` }} className={`infinite hidden md:flex scrollbar-none shrink-0  flex-col w-[10vw] mx-10   bg-[#7776bc] text-[#fffbdb] overflow-hidden justify-center items-center h-[110vh] `}>
             <motion.h1 style={{ height: `${height}` }} initial={{rotate:'-90deg'}} animate={{ translateY: `${scrollDirection === 'up' ? '-100%' : '100%'}` }} transition={{ duration: 5, repeat: Infinity, ease: "linear" }} className={`uppercase transition-transform ease-linear pointer-events-none shrink-0 font-[Melodrama] text-8xl w-max text-center justify-center rotate-[-90deg] items-center flex flex-col px-7 `}>
                 {text}
             </motion.h1>
--- src/routes/MainRoutes.jsx
+++ src/routes/MainRoutes.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Home from '../components/Home'
+import AboutPage from '../components/AboutPage'
+import ContactUs from '../components/ContactUs'
+import SkillsPage from '../components/SkillsPage'
+import ProjectPage from '../components/ProjectPage'
 
 const MainRoutes = () => {
   return (
     <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/contact" element={<ContactUs />} />
+        <Route path="/skill" element={<SkillsPage />} />
+        <Route path="/projects" element={<ProjectPage />} />
     </Routes>
   )
 }
